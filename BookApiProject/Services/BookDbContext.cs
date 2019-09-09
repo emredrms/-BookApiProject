dcc2f8d4c4d8cf0f28c4e127ce6eee2d7b392446
GRATIS\emrd01tr <drmsemre@gmail.com>
2019-09-10 00:01:11 +0300

    first commit

---
+++ BookApiProject/Services/BookDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BookApiProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApiProject.Services
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Review> Reviews { get; set; }
+        public virtual DbSet<Reviewer> Reviewers { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Country> Country { get; set; }
+        public virtual DbSet<BookAuthor> BookAuthors { get; set; }
+        public virtual DbSet<BookCategory> BookCategories { get; set; }
+
+    }
+}
